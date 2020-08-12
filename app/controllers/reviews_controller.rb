ea1b78743b24dcd98b43aa16e50474694e4cb91a
Matthieu Minier <minier.matthieu@outlook.com>
2020-08-12 16:03:39 +0200

    working wonderfully fine

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,6 +7,7 @@ before_action :set_restaurant, only: [:new, :create]
 
   def create
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
