4987bee795323a48192e534ca7a0120dfe60159a
Matthieu Minier <minier.matthieu@outlook.com>
2020-08-12 15:36:16 +0200

    review mvc ongoing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,12 @@
 class ReviewsController < ApplicationController
+before_action :set_restaurant, only: [:new, :create]
+
   def new
-    @review = Review.new(restaurant_id: @restaurant.id)
+    @review = Review.new
   end
 
   def create
-    @review = Review.new(restaurant_id: @restaurant.id)
+    @review = Review.new(review_params)
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
@@ -12,7 +14,17 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
